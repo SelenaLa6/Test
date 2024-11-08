a046f946db6d0401d9c7559050a2495ec3ba2645
SelenaLa6 <selenala066@gmail.com>
2024-11-07 20:49:28 -0500

    creating vue website

---
+++ portfolio/src/views/About.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+<html>
+    <p>
+        it's a me
+    </p>
+</html>
+</template>
+
+<style scoped></style>
---
+++ portfolio/src/views/Home.vue
@@ -0,0 +1,16 @@
+<script setup ls>
+import { RouterLink } from 'vue-router';
+</script>
+<template>
+<html>
+    <header style="color:lightblue;font-size:xx-large;font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;">
+        Hi
+    </header>
+    <p>
+        helloooo hello hello heloo hello hi hello
+    </p>
+</html>
+
+</template>
+
+<style scoped></style>
